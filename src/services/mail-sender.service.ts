bd14f61e6d1e49cdabcf3b6179580ef575c5fc11
ankit2103 <achawda866@gmail.com>
2025-04-07 20:06:07 +0530

    Update mail-sender.service.ts

--- src/services/mail-sender.service.ts
+++ src/services/mail-sender.service.ts
@@ -1,9 +1,10 @@
 import nodemailer from "nodemailer";
 
 export const MailSender = nodemailer.createTransport({
-     service: "gmail",
+     host: 'smtp.mailtrap.io',
+     port: 2525,
      auth: {
-          user: "alterbuddy8@gmail.com",
-          pass: "fhuoaoaoajsyakwq",
+       user: '55b2d9a2d90121', // get from Mailtrap
+       pass: 'f56b3e52d8e9a3', // get from Mailtrap
      },
 });
